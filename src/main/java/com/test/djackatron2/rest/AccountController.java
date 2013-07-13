ce46474a5acd9d899dbac22cec1f5e1a10800a84
piya_l <piya.l@g-able.com>
2013-07-14 00:20:49 +0700

    increase from 9tae

---
+++ src/main/java/com/test/djackatron2/rest/AccountController.java
@@ -0,0 +1,10 @@
+package com.test.djackatron2.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+}
