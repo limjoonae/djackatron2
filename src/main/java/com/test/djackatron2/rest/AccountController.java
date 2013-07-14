eb9d11337e7ca60945a3d672a5942c6e51548157
supakan <limjoonae.swk@gmail.com>
2013-07-14 11:51:30 +0700

    increase from 9tae

--- src/main/java/com/test/djackatron2/rest/AccountController.java
+++ src/main/java/com/test/djackatron2/rest/AccountController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
+	public Account getOne(){
+		return new Account
+	}
 }
